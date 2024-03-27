3b46b03ea76ce2a7b31eb644eb83d22564977aed
Rakulapplication <rakulj321@gmail.com>
2024-03-27 11:56:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [length,setLength] = useState(8);
+  const [includeUppercase,setIncludeUppercase] = useState(true);
+  const [includeLowercase,setIncludeLowercase] = useState(true);
+  const [includeNumbers,setIncludeNumbers] = useState(true);
+  const [includeSymbols,setIncludeSymbols] = useState(true);
+  const [ password,setPassword] = useState("");
+
+  const generatePassword = () =>{
+     let charset ="";
+     if(includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     if(includeLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+     if(includeNumbers) charset += "0123456789";
+     if(includeSymbols) charset += "!@#$%^&*()-_=+";
+     let generatePassword = "";
+     for(let i = 0; i < length; i++){
+       const randomIndex = Math.floor(Math.random() * charset.length);
+       generatePassword += charset[randomIndex];
+     }
+     setPassword(generatePassword);
+  };
+
+  const copyToClipboard = () =>{
+    navigator.clipboard.writeText(password);
+    alert("Password Copied");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="password-generator">
+    <h2>Strong Password Generator</h2>
+    <div className="input-group">
+      <label htmlFor="num">Password Length:</label>
+      <input type="number" id="num" value={length} onChange={(e)=>setLength(parseInt(e.target.value))}/>
     </div>
+    <div className="checkbox-group">
+      <input type="checkbox" id="upper" checked={includeUppercase} onChange={(e)=>setIncludeUppercase(e.target.checked)}/>
+      <label htmlFor="upper">Include Uppercase</label>   
+    </div>
+    <div className="checkbox-group">
+      <input type="checkbox" id="lower" checked={includeLowercase} onChange={(e)=>setIncludeLowercase(e.target.checked)}/>
+      <label htmlFor="lower">Include Lowercase</label>   
+    </div>
+    <div className="checkbox-group">
+      <input type="checkbox" id="numbers" checked={includeNumbers} onChange={(e)=>setIncludeNumbers(e.target.checked)}/>
+      <label htmlFor="numbers">Include Numbers</label>   
+    </div>
+    <div className="checkbox-group">
+      <input type="checkbox" id="symbol" checked={includeSymbols} onChange={(e)=>setIncludeSymbols(e.target.checked)}/>
+      <label htmlFor="symbol">Include Symbol</label>   
+    </div>
+    <button className="generate-btn" onClick={generatePassword}>Generator Password</button>
+    <div className="generated-password">
+      <input type="text" readOnly value={password}/>
+      <button className="copy-btn" onClick={copyToClipboard}>Copy</button>
+    </div>
+   </div>
   );
 }
 
 export default App;
+
